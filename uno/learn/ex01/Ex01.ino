c08c23a3428eda275c846ab8103ec6ff1ab35c52
Suvankar Mazumder <smazumder@gmail.com>
2017-02-21 00:19:01 -0500

    Update Ex01.ino

--- uno/learn/ex01/Ex01.ino
+++ uno/learn/ex01/Ex01.ino
@@ -1,16 +1,10 @@
-/* Blink without Delay
-
- Turns on and off a light emitting diode (LED) connected to a digital
- pin, without using the delay() function.  This means that other code
- can run at the same time without being interrupted by the LED code.
-
- The circuit:
- * LED attached from pin 3 to ground.
- * Note: on most Arduinos, there is already an LED on the board
- * that's attached to pin 13, so no hardware is needed for this example.
+/* 
+ * Blink without Delay
+ * Turns on and off a light emitting diode (LED) connected to a digital
+ * pin, without using the delay() function.  This means that other code
+ * can run at the same time without being interrupted by the LED code.
  * 
  */
-
 // constants won't change. Used here to set a pin number :
 const int ledPin =  3;      // the number of the LED pin
 
